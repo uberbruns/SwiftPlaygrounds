8c43a1369011800545c3b8be7d46010e2dfb946b
uberbruns <karsten@bruns.me>
2015-08-29 21:19:23 +0200

    Play play play

--- 15-08-Integers.playground/Contents.swift
+++ 15-08-Integers.playground/Contents.swift
@@ -2,18 +2,33 @@
 
 import UIKit
 
-var a = "Hello World".hash
-var b = "Moin".hash
+var a = 3
+var b = 2*2*2*2*2*2*2
 
 String(a, radix: 2, uppercase: false)
-
+String(b, radix: 2, uppercase: false)
 
 func twoIntHash(a:Int, _ b:Int) -> UInt64
 {
+    // Prepare Input
     let a64 = UInt64(abs(a) % Int(UInt32.max))
+    String(a64, radix: 2, uppercase: false)
+    
     let b64 = UInt64(abs(b) % Int(UInt32.max))
-    return a64 << 32 | b64
+    String(b64, radix: 2, uppercase: false)
+
+    // Hash
+    var hash: UInt64 = 0
+    String(hash, radix: 2, uppercase: false)
+
+    hash = a64 << 32
+    String(hash, radix: 2, uppercase: false)
+
+    hash |= b64
+    String(hash, radix: 2, uppercase: false)
+    
+    return hash
 }
 
-twoIntHash(a, b)
-
+let result = twoIntHash(a, b)
+String(result, radix: 2, uppercase: false)
