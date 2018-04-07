af93042b71825d6b7bb0539c12f809c8213647a2
Karsten Bruns <kbruns@MB0690.fritz.box>
2018-04-07 12:26:29 +0200

    add constraint extension

---
+++ 18-04-NSConstraintExtensions/NSConstraintExtensions/NSConstraint+Extensions.swift
@@ -0,0 +1,306 @@
+//
+//  NSConstraint+Extensions.swift
+//  NSConstraintExtensions
+//
+//  Created by Karsten Bruns on 07.04.18.
+//  Copyright © 2018 bruns.me. All rights reserved.
+//
+
+import UIKit
+
+extension NSLayoutConstraint {
+    
+    open static func toggleConstraints(_ constraints: [NSLayoutConstraint]) {
+        
+        var constraintsToActivate = [NSLayoutConstraint]()
+        var constraintsToDeactivate = [NSLayoutConstraint]()
+        
+        for constraint in constraints {
+            // Handle constraint for invisibile views
+            let viewIsHidden = (constraint.firstItem as? UIView)?.isInvisible == true
+            if constraint.identifier?.starts(with: ".") == true {
+                if viewIsHidden && !constraint.isActive {
+                    constraintsToActivate.append(constraint)
+                } else if !viewIsHidden && constraint.isActive {
+                    constraintsToDeactivate.append(constraint)
+                }
+                continue
+            }
+
+            if constraint.identifier?.starts(with: "~") == true {
+                // `true` if one participating view is hidden
+                let deactivateConstraint = [constraint.firstItem, constraint.secondItem]
+                    .compactMap({ $0 as? UIView })
+                    .contains(where: { $0.isInvisible })
+                
+                if deactivateConstraint && constraint.isActive {
+                    constraintsToDeactivate.append(constraint)
+                } else if !deactivateConstraint && !constraint.isActive {
+                    constraintsToActivate.append(constraint)
+                }
+                continue
+            }
+
+            // Handle permanent constraints
+            if !constraint.isActive {
+                constraintsToActivate.append(constraint)
+            }
+        }
+        
+        NSLayoutConstraint.activate(constraintsToActivate)
+        NSLayoutConstraint.deactivate(constraintsToDeactivate)
+    }
+}
+
+
+extension NSLayoutXAxisAnchor {
+    func constraint(equalTo anchor: NSLayoutXAxisAnchor, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    func constraint(greaterThanOrEqualTo anchor: NSLayoutXAxisAnchor, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    func constraint(lessThanOrEqualTo anchor: NSLayoutXAxisAnchor, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    func constraint(equalTo anchor: NSLayoutXAxisAnchor, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    func constraint(greaterThanOrEqualTo anchor: NSLayoutXAxisAnchor, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    func constraint(lessThanOrEqualTo anchor: NSLayoutXAxisAnchor, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    @available(iOS 11.0, *)
+    open func constraintEqualToSystemSpacingAfter(_ anchor: NSLayoutXAxisAnchor, multiplier: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraintEqualToSystemSpacingAfter(anchor, multiplier: multiplier)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    @available(iOS 11.0, *)
+    open func constraintGreaterThanOrEqualToSystemSpacingAfter(_ anchor: NSLayoutXAxisAnchor, multiplier: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraintGreaterThanOrEqualToSystemSpacingAfter(anchor, multiplier: multiplier)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    @available(iOS 11.0, *)
+    open func constraintLessThanOrEqualToSystemSpacingAfter(_ anchor: NSLayoutXAxisAnchor, multiplier: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraintLessThanOrEqualToSystemSpacingAfter(anchor, multiplier: multiplier)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+}
+
+
+extension NSLayoutYAxisAnchor {
+    func constraint(equalTo anchor: NSLayoutYAxisAnchor, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    func constraint(greaterThanOrEqualTo anchor: NSLayoutYAxisAnchor, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+
+    func constraint(lessThanOrEqualTo anchor: NSLayoutYAxisAnchor, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+
+    func constraint(equalTo anchor: NSLayoutYAxisAnchor, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+
+    func constraint(greaterThanOrEqualTo anchor: NSLayoutYAxisAnchor, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+
+    func constraint(lessThanOrEqualTo anchor: NSLayoutYAxisAnchor, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    @available(iOS 11.0, *)
+    open func constraintEqualToSystemSpacingBelow(_ anchor: NSLayoutYAxisAnchor, multiplier: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraintEqualToSystemSpacingBelow(anchor, multiplier: multiplier)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    @available(iOS 11.0, *)
+    open func constraintGreaterThanOrEqualToSystemSpacingBelow(_ anchor: NSLayoutYAxisAnchor, multiplier: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraintGreaterThanOrEqualToSystemSpacingBelow(anchor, multiplier: multiplier)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    @available(iOS 11.0, *)
+    open func constraintLessThanOrEqualToSystemSpacingBelow(_ anchor: NSLayoutYAxisAnchor, multiplier: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraintLessThanOrEqualToSystemSpacingBelow(anchor, multiplier: multiplier)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+}
+
+
+extension NSLayoutDimension {
+    open func constraint(equalTo anchor: NSLayoutDimension, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(greaterThanOrEqualTo anchor: NSLayoutDimension, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(lessThanOrEqualTo anchor: NSLayoutDimension, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualTo: anchor)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(equalTo anchor: NSLayoutDimension, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(greaterThanOrEqualTo anchor: NSLayoutDimension, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(lessThanOrEqualTo anchor: NSLayoutDimension, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualTo: anchor, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+
+    open func constraint(equalToConstant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalToConstant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(greaterThanOrEqualToConstant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualToConstant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(lessThanOrEqualToConstant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualToConstant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(equalTo anchor: NSLayoutDimension, multiplier m: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalTo: anchor, multiplier: m)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(greaterThanOrEqualTo anchor: NSLayoutDimension, multiplier m: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualTo: anchor, multiplier: m)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(lessThanOrEqualTo anchor: NSLayoutDimension, multiplier m: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualTo: anchor, multiplier: m)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(equalTo anchor: NSLayoutDimension, multiplier m: CGFloat, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(equalTo: anchor, multiplier: m)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(greaterThanOrEqualTo anchor: NSLayoutDimension, multiplier m: CGFloat, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(greaterThanOrEqualTo: anchor, multiplier: m)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+    
+    open func constraint(lessThanOrEqualTo anchor: NSLayoutDimension, multiplier m: CGFloat, constant c: CGFloat, identifier: String, priority: UILayoutPriority = .required) -> NSLayoutConstraint {
+        let newConstraint = constraint(lessThanOrEqualTo: anchor, multiplier: m, constant: c)
+        newConstraint.identifier = identifier
+        newConstraint.priority = priority
+        return newConstraint
+    }
+}
+
+
+private extension UIView {
+    var isInvisible: Bool {
+        return isHidden || alpha < 0.0001
+    }
+}
---
+++ 18-04-NSConstraintExtensions/NSConstraintExtensions/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  NSConstraintExtensions
+//
+//  Created by Karsten Bruns on 07.04.18.
+//  Copyright © 2018 bruns.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var redView = UIView()
+    private lazy var blueView = UIView()
+
+    private lazy var constraints = [
+        redView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, identifier: "redView.top"),
+        redView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, identifier: "~redView.bottom.whenBlueViewIsHidden", priority: .defaultLow),
+        redView.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, identifier: "redView.left"),
+        redView.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, identifier: "redView.right"),
+        redView.heightAnchor.constraint(equalToConstant: 0, identifier: ".redView.bottomAnchor"),
+
+        blueView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, identifier: "~blueView.top.whenRedViewIsHidden", priority: .defaultLow),
+        blueView.topAnchor.constraintEqualToSystemSpacingBelow(redView.bottomAnchor, multiplier: 1, identifier: "~blueView.top"),
+        blueView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, identifier: "blueView.bottom"),
+        blueView.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, identifier: "blueView.left"),
+        blueView.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, identifier: "blueView.right"),
+        blueView.heightAnchor.constraint(equalTo: redView.heightAnchor, identifier: "~blueView.height"),
+        blueView.heightAnchor.constraint(equalToConstant: 0, identifier: ".blueView.height"),
+        ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        view.setNeedsUpdateConstraints()
+    }
+
+    private func addSubviews() {
+        redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapGesture)))
+        redView.backgroundColor = .red
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(redView)
+        
+        blueView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapGesture)))
+        blueView.backgroundColor = .blue
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(blueView)
+    }
+
+    override func updateViewConstraints() {
+        NSLayoutConstraint.toggleConstraints(constraints)
+        super.updateViewConstraints()
+    }
+    
+    @objc private func handleTapGesture(_ gesture: UITapGestureRecognizer) {
+        UIView.animate(withDuration: 1/3) { [weak self] in
+            guard let redView = self?.redView, let blueView = self?.blueView else { return }
+            
+            if gesture.view == blueView {
+                if redView.alpha == 0 {
+                    redView.alpha = 1
+                } else {
+                    blueView.alpha = 0
+                }
+            }
+            
+            if gesture.view == redView {
+                if blueView.alpha == 0 {
+                    blueView.alpha = 1
+                } else {
+                    redView.alpha = 0
+                }
+            }
+
+            self?.view.setNeedsUpdateConstraints()
+            self?.view.updateConstraintsIfNeeded()
+            self?.view.setNeedsLayout()
+            self?.view.layoutIfNeeded()
+        }
+    }
+}
+
