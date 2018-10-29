d55110cb143512a99f2e22a4e42c2e0acd938728
Karsten Bruns <karsten@bruns.me>
2018-10-29 18:01:59 +0100

    rename tests

--- 18-10-StickyLayout/StickyLayoutTests/CollectionViewFillLayoutTests.swift
+++ 18-10-StickyLayout/StickyLayoutTests/CollectionViewFillLayoutTests.swift
@@ -10,7 +10,7 @@ import Foundation
 import XCTest
 @testable import StickyLayout
 
-class StickyLayoutTests: XCTestCase {
+class CollectionViewFillLayoutTests: XCTestCase {
 
     override func setUp() {
     }
